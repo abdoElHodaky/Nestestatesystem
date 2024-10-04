195da58d24b7c21be6e1f97d8a036fb210e4cce1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-04 16:09:58 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import {useEffect} from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { appRoutes } from "./routes";
-
+import {registerSW} from "./swreg";
 function App() {
+  useEffect(()=>{
+    registerSW()
+  },[])
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
