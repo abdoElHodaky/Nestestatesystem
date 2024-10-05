cb7368970f23b4537633999e415bb96b52a19bfc
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-05 15:36:50 +0300

    Update database.ts

--- server/src/configs/database.ts
+++ server/src/configs/database.ts
@@ -12,7 +12,7 @@ export class DatabaseConfigService implements TypeOrmOptionsFactory {
     return {
       type: 'mysql',
       host: this.config.get('DB_HOST')||process.env.DB_HOST,
-      port: this.config.get('DB_PORT')||process.env.DB_PORT,
+      port: this.config.get('DB_PORT')||process.env.DB_PORT.toString(),
       username: this.config.get('MYSQL_DATABASE_USER_NAME')||process.env.MYSQL_USER_NAME,
       password: this.config.get('MYSQL_ROOT_PASSWORD')||process.env.MYSQL_PASSWORD,
       database: this.config.get('MYSQL_DATABASE')||process.env.MYSQL_DATABASE,
