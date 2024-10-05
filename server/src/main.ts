3f0caccdf9ba67ee685fc8d2ec6944890dbbd827
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-05 21:08:44 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,10 +5,12 @@ import { SwaggerBuild } from './configs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const staticDirectory = `${process.cwd()}/client/dist/`;
   app.enableCors();
   SwaggerBuild(app);
   app.setGlobalPrefix('/api');
   app.useGlobalInterceptors(new MiddlewareInterceptor());
+  app.useStaticAssets(staticDirectory);
   app.listen(process.env.NODE_SERVER_PORT,process.env.APP_URL).
     then(()=>console.log("Listen")).catch(console.log);
 }
