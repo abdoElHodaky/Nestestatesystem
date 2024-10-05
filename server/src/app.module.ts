59d27cdada92bf9a56c24e8ded6b7ee8d343dd59
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-05 14:09:12 +0300

    Update app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -15,7 +15,7 @@ import { join } from 'path';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: DatabaseConfigService }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../', 'client/dist/'),
+      rootPath: process.pwd()+'../'+'client/dist/' || "../client/dist/",
       exclude: ['/api/(.*)'],
     }),
     AuthModule,
