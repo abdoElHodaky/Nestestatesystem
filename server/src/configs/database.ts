46dd5b4a1c51d4f136ff0c17cddc5bf9856e80fa
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-05 15:35:11 +0300

    Update database.ts

--- server/src/configs/database.ts
+++ server/src/configs/database.ts
@@ -8,14 +8,14 @@ var redisParser = require('redis-url-parser');
 export class DatabaseConfigService implements TypeOrmOptionsFactory {
   constructor(@Inject(ConfigService) private readonly config: ConfigService) {}
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-   console.log(redisParser.parse(this.config.get("REDIS_URL")))
+   console.log(redisParser.parse(this.config.get("REDIS_URL")||process.env.REDIS_URL))
     return {
       type: 'mysql',
-      host: this.config.get('DB_HOST'),
-      port: this.config.get('DB_PORT'),
-      username: this.config.get('MYSQL_DATABASE_USER_NAME'),
-      password: this.config.get('MYSQL_ROOT_PASSWORD'),
-      database: this.config.get('MYSQL_DATABASE'),
+      host: this.config.get('DB_HOST')||process.env.DB_HOST,
+      port: this.config.get('DB_PORT')||process.env.DB_PORT,
+      username: this.config.get('MYSQL_DATABASE_USER_NAME')||process.env.MYSQL_USER_NAME,
+      password: this.config.get('MYSQL_ROOT_PASSWORD')||process.env.MYSQL_PASSWORD,
+      database: this.config.get('MYSQL_DATABASE')||process.env.MYSQL_DATABASE,
       cache:{
         type:"database",
         //options:{ ...redisParser.parse(this.config.get("REDIS_URL"))},
